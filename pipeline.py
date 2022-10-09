fcfea1ae4380be93164dcc7b82d46f3b65ebd955
geeno17 <32551074+geeno17@users.noreply.github.com>
2022-10-09 19:50:05 +0200

    Add files via upload

--- pipeline.py
+++ pipeline.py
@@ -6,22 +6,10 @@ from kfp.v2.dsl import pipeline
 from kfp.v2.dsl import component
 from kfp.v2 import compiler
 
-@component() 
-def concat(a: str, b: str) -> str:
-  return a + b
-
-@component
-def reverse(a: str)->NamedTuple("outputs", [("before", str), ("after", str)]):
-  return a, a[::-1]
-
-@pipeline(name="basic-pipeline",
-description="A simple intro pipeline", 
-              pipeline_root='gs://doit-vertex-demo/basic-pipeine')
-def basic_pipeline(a: str='stres', b: str='sed'):
-    concat_task = concat(a, b)
-    reverse_task = reverse(concat_task.output)
-
 if __name__ == '__main__':
-    compiler.Compiler().compile(
-        pipeline_func=basic_pipeline, package_path="basic_pipeline.json"
-    )
+    data = json.load(open('config.json'))
+    f = open("demo.txt", "w")
+    f.write(str(data['key3']))
+    f.write(str(data['key2']))
+    f.write(str(data['key1']))
+    f.close()
